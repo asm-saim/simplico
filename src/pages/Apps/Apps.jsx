2ae73d463587436754a5271fc89fa787dfd76865
asm-saim <saim777m@gmail.com>
2026-07-21 08:30:15 +0600

    All Apps title and details implemented

--- src/pages/Apps/Apps.jsx
+++ src/pages/Apps/Apps.jsx
@@ -8,6 +8,10 @@ const Apps = () => {
 
     return (
         <div className='max-w-7xl mx-auto'>
+            <div className='text-center space-y-3 mt-5 mb-10'>
+                <h1 className='font-semibold text-5xl'>Our All Applications</h1>
+                <p className='italic text-gray-400'>Explore All Apps on the Market developed by us. We code for Millions</p>
+            </div>
             <div className='grid grid-cols-4 gap-10 my-10'>
                 {
                     allApps.map(app => <App key={app.key} app={app} ></App>)
