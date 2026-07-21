4e162857e099595f2b5ea058ba5217fd3b01c231
asm-saim <saim777m@gmail.com>
2026-07-21 08:49:29 +0600

    all apps length desplayed in UI and search apps place initiated

--- src/pages/Apps/Apps.jsx
+++ src/pages/Apps/Apps.jsx
@@ -8,9 +8,13 @@ const Apps = () => {
 
     return (
         <div className='max-w-7xl mx-auto'>
-            <div className='text-center space-y-3 mt-5 mb-10'>
+            <div className='text-center space-y-3 mt-5 mb-5'>
                 <h1 className='font-semibold text-5xl'>Our All Applications</h1>
                 <p className='italic text-gray-400'>Explore All Apps on the Market developed by us. We code for Millions</p>
+            </div>
+            <div className='flex justify-between items-center'>
+                <p className='font-semibold text-xl'>({allApps.length}) Apps Found</p>
+                <p className=' text-base text-gray-200 border border-gray-600 pr-20 pl-3  rounded-md py-1'>search Apps</p>
             </div>
             <div className='grid grid-cols-4 gap-10 my-10'>
                 {
