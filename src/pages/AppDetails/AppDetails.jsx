8da2840c10da3e2b613a47a617a59ef1f00566ea
asm-saim <saim777m@gmail.com>
2026-07-21 14:26:51 +0600

    app detail pages first part completed

--- src/pages/AppDetails/AppDetails.jsx
+++ src/pages/AppDetails/AppDetails.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { useLoaderData, useParams } from 'react-router';
 
+import downloadImg from "../../assets/icon-downloads.png"
+import ratingImg from "../../assets/icon-ratings.png"
+import reviewsImg from "../../assets/icon-review.png"
+
 const AppDetails = () => {
 
     //grab the clicked card id
@@ -19,7 +23,26 @@ const AppDetails = () => {
     }
 
     //destructuring:
-    const { image, title, companyName, description, reviews, ratingAvg } = singleAppDetail;
+    const { image, title, companyName, description, reviews, ratingAvg, downloads } = singleAppDetail;
+
+    //convert numbers to K  M  B:
+    const formatDownloads = (downloads) => {
+        if (downloads >= 1000000000) {
+            return +(downloads / 1000000000).toFixed(1) + "B";
+        }
+
+        if (downloads >= 1000000) {
+            return +(downloads / 1000000).toFixed(1) + "M";
+        }
+
+        if (downloads >= 1000) {
+            return +(downloads / 1000).toFixed(1) + "K";
+        }
+
+        return downloads.toString();
+    };
+
+
     // {
     //     "image": "https://i.ibb.co/forest-app.png",
     //     "title": "Forest: Focus for Productivity",
@@ -57,17 +80,41 @@ const AppDetails = () => {
 
 
     return (
-        <div className='max-w-7xl mx-auto'>
-            <div className='flex border gap-16'>
-                <div>
+        <div className='max-w-7xl mx-auto border'>
+            <div className='flex my-10'>
+                <div className='w-1/3'>
                     <img src={image} className='w-90' alt="" />
                 </div>
-                <div className='space-y-4'>
+                <div className='w-2/3 space-y-4'>
                     <h1 className='text-4xl font-bold'>{title}</h1>
-                    <h2 className='text-xl font-semibold'>Developed by: 
+                    <h2 className='text-xl font-semibold'>Developed by:
                         <span className='text-blue-400 font-bold'> {companyName}</span> </h2>
+                    <hr className="my-5 border-gray-500 mr-5" />
+
+                    <div className='grid grid-cols-3 mt-10'>
+                        <div className='space-y-2'>
+                            <img src={downloadImg} alt="" />
+                            <h1 className='text-gray-300'>Downloads</h1>
+                            <p className='text-4xl font-bold'>{formatDownloads(downloads)}</p>
+                        </div>
+                        <div className='space-y-2'>
+                            <img src={ratingImg} alt="" />
+                            <h1 className='text-gray-300'>Average Rating</h1>
+                            <p className='text-4xl font-bold'>{ratingAvg}</p>
+                        </div>
+                        <div className='space-y-2'>
+                            <img src={reviewsImg} alt="" />
+                            <h1 className='text-gray-300'>Total Reviews</h1>
+                            <p className='text-4xl font-bold'>{formatDownloads(reviews)}</p>
+                        </div>
+                    </div>
+                    <button className="btn border-none rounded-lg text-base font-semibold bg-gradient-to-br from-[#0B2F5B] to-[#3B82F6] my-5">
+                        Install Now (240MB)
+                    </button>
                 </div>
             </div>
+
+            <hr className="my-5 border-gray-500" />
 
         </div>
     );
