f8e3b5b2c9de12988304244af562b5ade77a7a69
asm-saim <saim777m@gmail.com>
2026-07-23 23:04:21 +0600

    sorted feature implemented

--- src/pages/Installation/Installation.jsx
+++ src/pages/Installation/Installation.jsx
@@ -28,9 +28,37 @@ const Installation = () => {
         setInstalledList(remaining);
     };
 
+    //sort:
+    const [sort, setSort] = useState("")
+
+    const handelSort = (type) => {
+        setSort(type)
+
+        if (type === "high-low") {
+            const sortByHighToLow = [...installedList].sort((a,b) => b.downloads - a.downloads);
+            setInstalledList(sortByHighToLow)
+        }
+
+        if (type === "low-high") {
+            const sortByLowToHigh = [...installedList].sort((a, b) => a.downloads - b.downloads)
+            setInstalledList(sortByLowToHigh)
+        }
+    }
+
     return (
-        <div className='mt-5 mb-10'>
-            <h1 className='mb-5 text-xl font-semibold'>{installedList.length} Apps Found</h1>
+        <div className=''>
+
+            <div className='flex justify-between items-center my-5'>
+                <h1 className='text-xl font-semibold'>{installedList.length} Apps Found</h1>
+                <details className="dropdown ">
+                    <summary className="btn m-1 border border-gray-400 text-base rounded-md">Sort by Downloads</summary>
+                    <ul className="menu dropdown-content bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+                        <li><a onClick={() => handelSort("high-low")}>HIGHT-LOW</a></li>
+                        <li><a onClick={() => handelSort("low-high")}>LOW-HIGH</a></li>
+                    </ul>
+                </details>
+            </div>
+
             {
                 installedList.map(app => <Installed key={app.id} app={app}
                     handleUninstall={handleUninstall}
