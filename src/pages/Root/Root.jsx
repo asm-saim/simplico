6044ed798444429275f01ff8024568fe3f81fc01
asm-saim <saim777m@gmail.com>
2026-07-23 09:57:36 +0600

    implemented loader on outlet

--- src/pages/Root/Root.jsx
+++ src/pages/Root/Root.jsx
@@ -1,13 +1,33 @@
 import React from 'react';
 import Navbar from '../../components/Header/Navbar';
-import { Outlet } from 'react-router';
+import { Outlet, useNavigation } from 'react-router';
 import Footer from '../../components/Footer/Footer';
+import { Bars, ColorRing } from 'react-loader-spinner';
 
 const Root = () => {
+
+    const navigation = useNavigation();
+
     return (
         <>
             <Navbar></Navbar>
-            <Outlet></Outlet>
+            <div className='max-w-7xl mx-auto'>
+                {
+                    navigation.state === "loading" ?
+                        (<div className='flex justify-center items-center min-h-[50vh]'>
+                            <ColorRing
+                                visible={true}
+                                height="80"
+                                width="80"
+                                ariaLabel="color-ring-loading"
+                                wrapperStyle={{}}
+                                wrapperClass="color-ring-wrapper"
+                                colors={['#3B82F6', '#3B82F6', '#3B82F6', '#3B82F6', '#3B82F6']}
+                            />
+                        </div>)
+                        : (<Outlet></Outlet>)
+                }
+            </div>
             <Footer></Footer>
         </>
     );
