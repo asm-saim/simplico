0cc051298643af39040eb7a9f95059178a9bf57e
asm-saim <saim777m@gmail.com>
2026-07-25 09:27:42 +0600

    search input added

--- src/pages/Apps/Apps.jsx
+++ src/pages/Apps/Apps.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { useLoaderData } from 'react-router';
 import App from '../App/App';
+import { useState } from 'react';
 
 const Apps = () => {
 
     const allApps = useLoaderData()
+
+    const [search, setSearch] = useState("");
+
+    const filteredApps = allApps.filter(app => app.title.toLowerCase().includes(search.toLowerCase()))
 
     return (
         <div className='max-w-7xl mx-auto'>
@@ -13,12 +18,19 @@ const Apps = () => {
                 <p className='italic text-gray-400'>Explore All Apps on the Market developed by us. We code for Millions</p>
             </div>
             <div className='flex justify-between items-center'>
-                <p className='font-semibold text-xl'>({allApps.length}) Apps Found</p>
-                <p className=' text-base text-gray-200 border border-gray-600 pr-20 pl-3  rounded-md py-1'>search Apps</p>
+                <p className='font-semibold text-xl'>({filteredApps.length}) Apps Found</p>
+                <input
+                    type="text"
+                    placeholder="Search apps..."
+                    value={search}
+                    onChange={(e) => setSearch(e.target.value)}
+                    className="input input-bordered w-80 text-base focus:outline-none focus:ring-0 focus:shadow-none focus:border-gray-600"
+                />
+
             </div>
             <div className='grid grid-cols-4 gap-10 my-10'>
                 {
-                    allApps.map(app => <App key={app.id} app={app} ></App>)
+                    filteredApps.map(app => <App key={app.id} app={app} ></App>)
                 }
             </div>
         </div>
